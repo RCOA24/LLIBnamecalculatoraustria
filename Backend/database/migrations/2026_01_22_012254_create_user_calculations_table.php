c63d4a0afccf5531a20fd451f5a12ec93912dc0f
Rodney Austria <mrtangkad59@gmail.com>
2026-01-22 09:32:35 +0800

    table schema set up

--- Backend/database/migrations/2026_01_22_012254_create_user_calculations_table.php
+++ Backend/database/migrations/2026_01_22_012254_create_user_calculations_table.php
@@ -9,13 +9,15 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('user_calculations', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('user_calculations', function (Blueprint $table) {
+        $table->id();
+        $table->string('full_name');
+        $table->integer('calculation_result'); // Result stored here
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
